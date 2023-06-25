96d0102bafbd143627fed873d401ebd6dca40ea5
Fuyuki_Vila <1642421711@qq.com>
2023-06-25 21:30:07 +0800

    update

--- Plugin/work.lua
+++ Plugin/work.lua
@@ -24,20 +24,20 @@ end
 
 function work(msg)
     local boss = getAtQQ(string.match(msg.fromMsg, "^[%s]*(.-)[%s]*$", #"打工" + 1))
+    if (tonumber(boss) == nil) then
+        return ""
+    end
     if (msg.gid == "") then
         return "私聊窗口不能打工捏×"
     end
     if (getUserToday(msg.uid, "work", 0) >= workLimit) then
         return "您今天不能再打工了×"
     end
-    if (getUserConf(msg.uid, "money", 0) >= getUserConf(boss,"money",0)) then
+    if (getUserConf(msg.uid, "money", 0) >= getUserConf(boss, "money", 0)) then
         return "给穷鬼打工？"
     end
-    if (tonumber(boss) == nil) then
-        return ""
-    end
-    if (getUserConf(boss, "money", 0) < 50) then
-        return "老板也没钱捏×"
+    if (getUserConf(msg.uid, "money", 0) >= 100) then
+        return "这么有钱还打工？"
     end
     setUserConf(boss, "request", 1)
     setUserConf(boss, "accept", 0)
@@ -49,7 +49,7 @@ function work(msg)
             setUserConf(boss, "request", 0)
             setUserToday(msg.uid, "work", getUserToday(msg.uid, "work", 0) + 1)
             local res
-            if (ranint(1, 100) <= 5) then
+            if (ranint(1, 100) <= 10) then
                 res = "老板很中意你，你变成了老板的星怒，并将一半的财产给了你"
                 local money = getUserConf(boss, "money", 0)
                 setUserConf(boss, "money", money / 2)
@@ -58,13 +58,13 @@ function work(msg)
             else
                 res = drawDeck(0, msg.uid, "_work")
                 local money = getUserConf(boss, "money", 0)
-                local getMoney = ranint(10, math.min(100, money * 2))
+                local getMoney = ranint(10, 50)
                 res = res .. "，你为老板创造了" .. getMoney .. "的价值\n"
                 local r = ranint(1, 100)
-                if r <= 5 then
+                if r <= 20 then
                     res = res .. "老板大发慈悲，把钱都给了你"
                     addMoney(msg.uid, getMoney)
-                elseif r <= 20 then
+                elseif r <= 40 then
                     res = res .. "老板将60%的利润给了你"
                     addMoney(msg.uid, getMoney * 0.6)
                     addMoney(boss, getMoney * 0.4)
@@ -72,7 +72,7 @@ function work(msg)
                     res = res .. "老板将30%的利润给了你"
                     addMoney(msg.uid, getMoney * 0.3)
                     addMoney(boss, getMoney * 0.7)
-                elseif r <= 95 then
+                elseif r <= 80 then
                     res = res .. "你干的不错，但是老板只给了你10%的利润"
                     addMoney(msg.uid, getMoney * 0.1)
                     addMoney(boss, getMoney * 0.9)
